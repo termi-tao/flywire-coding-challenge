f77c921f323d438c891692d9927da8233eff12e0
Tao Zhang <tao.zh2020@gmail.com>
2024-08-30 22:55:04 +0930

    refined models

--- apps/admission/models.py
+++ apps/admission/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=50, default="unnamed-course")
+    name = models.CharField(max_length=50, unique=True)
 
 
 class Intake(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="intakes")
     start_date = models.DateField(default="1800-01-01")
     end_date = models.DateField(default="1800-12-31")
