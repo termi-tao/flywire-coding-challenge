c0461a02c768efe0bdbd3e7e12c9f1a75500183a
Tao Zhang <tao.zh2020@gmail.com>
2024-08-30 22:37:33 +0930

    add models and migration file

--- apps/admission/models.py
+++ apps/admission/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Course(models.Model):
-    pass
+    name = models.CharField(max_length=50, default="unnamed-course")
 
 
 class Intake(models.Model):
-    pass
+    start_date = models.DateField(default="1800-01-01")
+    end_date = models.DateField(default="1800-12-31")
