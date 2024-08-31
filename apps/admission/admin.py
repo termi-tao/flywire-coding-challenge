e3f021fa50c34236f04d318550e01aa77c4598ed
Tao Zhang <tao.zh2020@gmail.com>
2024-08-31 18:13:49 +0930

    restructured

--- apps/admission/admin.py
+++ apps/admission/admin.py
@@ -1,13 +1,36 @@
 from django.contrib import admin
 from .models import Course, Intake
-from .utils import display_course_intakes, display_intake_course, view_link
+from .utils import entry_operations, get_admin_url, get_view_link
+from django.utils.html import format_html_join
+
 # Register your models here.
+
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ["name", display_course_intakes]
+    list_display = ["name", "display_course_intakes", entry_operations]
+
+    @admin.display(description="Intakes")
+    def display_course_intakes(self, course):
+        """Display course intakes in hyperlinks which are separated by commas.
+        Each intake is represented by its own start date.
+        """
+        course_intakes = course.intakes.all()
+        return format_html_join(
+            ", ",
+            "<a href='{}'>{}</a>",
+            (
+                (get_admin_url(intake, "change"), intake.start_date)
+                for intake in course_intakes
+            ),
+        )
 
 
 @admin.register(Intake)
 class IntakeAdmin(admin.ModelAdmin):
-    list_display = [display_intake_course, "start_date", "end_date", view_link]
+    list_display = ["display_intake_course", "start_date", "end_date", entry_operations]
+
+    @admin.display(description="Course")
+    def display_intake_course(self, intake):
+
+        return get_view_link(intake.course, intake.course.name, "")
--- apps/admission/utils/admin_utils.py
+++ apps/admission/utils/admin_utils.py
@@ -1,33 +1,28 @@
 from django.contrib import admin
 from django.urls import reverse
-from django.utils.html import format_html, format_html_join
-from functools import reduce
+from django.utils.html import format_html
 
 
-def get_admin_url(obj, view_name):
+def get_admin_url(obj: admin.ModelAdmin, view_name: str) -> str:
     opts = obj._meta
-    return reverse(f"admin:{opts.app_label}_{opts.model_name}_{view_name}", args=[obj.pk])
-
-
-@admin.display(description="Intakes")
-def display_course_intakes(course):
-    """ Display course intakes in hyperlinks which are separated by commas.
-        Each intake is represented by its own start date.
-    """
-    course_intakes = course.intakes.all()
-    return format_html_join(
-        ", ",
-        "<a href='{}'>{}</a>",
-        ((get_admin_url(intake, "change"), intake.start_date) for intake in course_intakes)
+    return reverse(
+        f"admin:{opts.app_label}_{opts.model_name}_{view_name}", args=[obj.pk]
     )
 
 
-@admin.display(description="Course")
-def display_intake_course(intake):
-    return view_link(intake.course, intake.course.name)
+def get_view_link(obj: admin.ModelAdmin, text: str ="View", css_class: str ="viewlink") -> str:
+    return get_link(obj, "change", text, css_class)
 
 
-@admin.display(description="Details")
-def view_link(obj, text="View"):
-    url = get_admin_url(obj, "change")
-    return format_html("<a href='{}'>{}</a>", url, text)
+def get_delete_link(obj: admin.ModelAdmin, text: str ="Delete", css_class: str ="deletelink") -> str:
+    return get_link(obj, "change", text, css_class)
+
+
+def get_link(obj: admin.ModelAdmin, action: str , text: str , css_class: str ) -> str:
+    url = get_admin_url(obj, action)
+    return format_html("<a class='{}' href='{}'>{}</a>", css_class, url, text)
+
+
+@admin.display(description="Operations")
+def entry_operations(obj: admin.ModelAdmin) -> str:
+    return format_html("<span>{}</span> | <span>{}</span>", get_view_link(obj), get_delete_link(obj))
