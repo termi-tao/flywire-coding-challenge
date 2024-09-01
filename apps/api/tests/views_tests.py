694f0f52a33ce9352078811ce76152dabccdd02d
Tao Zhang <tao.zh2020@gmail.com>
2024-09-01 16:07:25 +0930

    added more test cases

--- apps/api/tests/views_tests.py
+++ apps/api/tests/views_tests.py
@@ -1,19 +1,35 @@
 from rest_framework.test import APITestCase
 from apps.admission.models import Course
 from django.core.management import call_command
-from apps.api.views import ListCourses
 from rest_framework import status
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class ListCoursesTests(APITestCase):
     def setUp(self):
         call_command("loaddata", "../fixtures/api_test_data.json")
+        self.url = reverse("api:list_courses")
         self.courses = Course.objects.all()
-        view = ListCourses()
-        self.response = view.get({})
+        # create a test user for authentication purpose
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
 
-    def test_course_count(self):
+
+    def test_authenticated_access(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_unauthenticated_access(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_list_course(self):
+        self.client.force_authenticate(user=self.user)
         course_count = len(self.courses)
-        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
-        self.assertEqual(course_count, len(self.response.data))
-        self.assertEqual(self.response.data[0]["name"], "INFT101") # per api_test_data.json
+        response = self.client.get(self.url)
+        self.assertEqual(course_count, len(response.data))
+        self.assertEqual(response.data[0]["name"], "INFT101") # per api_test_data.json
