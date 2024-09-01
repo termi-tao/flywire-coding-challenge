12420963aa9fac5857c724836c56cf9deefba49c
Tao Zhang <tao.zh2020@gmail.com>
2024-09-01 16:37:54 +0930

    implemented list_course related endpoints and functions

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
+from apps.admission.models import Course
+from apps.admission.models import Intake
+
+
+class IntakeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Intake
+        fields = ['id', 'start_date', 'end_date']  
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    pass
+    # Include respective intakes
+    intakes = IntakeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ["name", "intakes"]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,7 +1,19 @@
 from rest_framework.views import APIView
-
 from .serializers import CourseSerializer
+from ..admission.models import Course
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class ListCourses(APIView):
-    serializer_class = CourseSerializer
+    # TODO: May use a custom authenticator to enable authentication throttling and such
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+    def get(self, request) -> Response:
+        # TODO: Integrate pagination when model expands
+        queryset = Course.objects.all()
+        serializer = CourseSerializer(queryset, many=True)
+        return Response(serializer.data)
